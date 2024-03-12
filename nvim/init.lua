c0079a1aab853d2b8719352ed65da72eea87ff4c
rboddy <avidryanb@gmail.com>
2024-03-12 09:48:56 -0400

    Neo tree is working and plugins are split out into their own file

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,23 +17,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-{ 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' }},
-{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    }, 
-}
-}
+
 local opts = {}
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 local builtin = require("telescope.builtin")
 
 vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
