bd42be97d6e32c5b558afb5f59c60dc3b0553990
rboddy <avidryanb@gmail.com>
2024-03-12 12:06:56 -0400

    Finally, formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
-    
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -17,24 +17,20 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
 local opts = {}
 
 require("lazy").setup("plugins")
 local builtin = require("telescope.builtin")
 
-vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>m', ':Neotree close<CR>', {})
+vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>m", ":Neotree close<CR>", {})
 local config = require("nvim-treesitter.configs")
 
 config.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-
-require("catppuccin").setup()
-vim.cmd.colorscheme = "catppuccin"
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html" },
+  sync_install = false,
+  highlight = { enable = true },
+  indent = { enable = true },
+})
