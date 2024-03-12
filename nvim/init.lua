75ebc3071cdc256e240a4f7944f7ddb8dc77690e
rboddy <avidryanb@gmail.com>
2024-03-12 10:44:20 -0400

    Adding in a keymap to close neotree quickly

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,7 +26,7 @@ local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
+vim.keymap.set('n', '<leader>m', ':Neotree close<CR>', {})
 local config = require("nvim-treesitter.configs")
 
 config.setup({
