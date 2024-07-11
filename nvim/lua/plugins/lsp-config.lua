e81f73dcc0207baeca338e99c3449962d5cd2d97
rboddy <avidryanb@gmail.com>
2024-07-11 09:43:36 -0400

    Adding in go support

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -19,6 +19,7 @@ return {
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
       capabilities.textDocument.completion.completionItem.snippetSupport = true
+      local util = require "lspconfig/util"
 
       local lspconfig = require("lspconfig")
       lspconfig.tsserver.setup({
@@ -33,6 +34,21 @@ return {
       lspconfig.svelte.setup{}
       lspconfig.cssls.setup{
         capabilities = capabilities
+      }
+      lspconfig.gopls.setup{
+        capabilities = capabilities,
+        cmd = {"gopls"},
+        filetypes = {"go", "gomod", "gowork", "gotmpl"},
+        root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+        settings = {
+          gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+              unusedparams = true,
+            },
+          },
+        },
       }
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -9,6 +9,8 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.formatting.golines,
         require("none-ls.diagnostics.eslint_d"),
 			},
 		})
